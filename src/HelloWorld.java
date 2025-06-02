b8f071b4800e156d9e7b088495f4c048ad51ca8b
yathu24 <arshblack22@gmail.com>
2025-06-02 14:29:43 +0530

    Added author's name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
     public static void main (String[ args]){
         System.out.println("Hello from GitHub and Java!");
+        System.out.println("This code was written by kajavathany");
         )
     }
 }
